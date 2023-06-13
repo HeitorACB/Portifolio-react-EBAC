e930257fa853591f87682e452afd38f0c667499d
heitor augusto <heitoraugusto8@hotmail.com>
2023-06-13 10:22:54 -0300

    first commi

---
+++ src/Testando.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Botao = styled.button`
+  background-color: red;
+`
+
+function Testando() {
+  return <Botao>clique aqui</Botao>
+}
+export default Testando
